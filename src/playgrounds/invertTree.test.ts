cfb3f8b2e53f9d57c0e6301627d327f9a86144ac
Thomas Scharke <scharke@van.dermeer.de>
2023-10-05 17:33:58 +0200

    chore: Add types

--- src/playgrounds/invertTree.test.ts
+++ src/playgrounds/invertTree.test.ts
@@ -1,29 +1,29 @@
 type TNode = {
-  value: string;
-  left?: TNode;
-  right?: TNode;
+	value: string;
+	left?: TNode;
+	right?: TNode;
 };
 
 type Tree = TNode;
 
-const invertTree = (node: TNode) => {
-  if (!node) {
-    return node;
-  }
+const invertTree = (node?: TNode): TNode | undefined => {
+	if (!node) {
+		return undefined;
+	}
 
-  const { value, left, right } = node;
+	const { value, left, right } = node;
 
-  const leftNode = invertTree(left);
-  const rightNode = invertTree(right);
+	const leftNode = invertTree(left);
+	const rightNode = invertTree(right);
 
-  return {
-    value,
-    // Conditionally add properties/keys to this object
-    ...(leftNode && { right: leftNode }),
-    ...(rightNode && { left: rightNode })
-  };
+	return {
+		value,
+		// Conditionally add properties/keys to this object
+		...(leftNode && { right: leftNode }),
+		...(rightNode && { left: rightNode }),
+	};
 
-  /*
+	/*
     This approche returns a of `undefined`-properties.
     Like…
 
@@ -32,7 +32,7 @@ const invertTree = (node: TNode) => {
        left: undefined,
        right: undefined,
     }
-    
+
     return {
       value,
       left: rightNode,
@@ -41,31 +41,31 @@ const invertTree = (node: TNode) => {
     */
 };
 
-test("invert tree", () => {
-  const tree: Tree = {
-    value: "root",
-    left: {
-      value: "1. left node",
-      left: {
-        value: "1.1. left mode"
-      }
-    },
-    right: {
-      value: "1. right node"
-    }
-  };
+test('invert tree', () => {
+	const tree: Tree = {
+		value: 'root',
+		left: {
+			value: '1. left node',
+			left: {
+				value: '1.1. left mode',
+			},
+		},
+		right: {
+			value: '1. right node',
+		},
+	};
 
-  const result = invertTree(tree);
-  expect(result).toEqual({
-    value: "root",
-    left: {
-      value: "1. right node"
-    },
-    right: {
-      value: "1. left node",
-      right: {
-        value: "1.1. left mode"
-      }
-    }
-  });
+	const result = invertTree(tree);
+	expect(result).toEqual({
+		value: 'root',
+		left: {
+			value: '1. right node',
+		},
+		right: {
+			value: '1. left node',
+			right: {
+				value: '1.1. left mode',
+			},
+		},
+	});
 });
