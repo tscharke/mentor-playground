761b2bd14db51f09a202cd57ec7e9ea97b8cc9fe
Thomas Scharke <scharke@van.dermeer.de>
2023-10-05 17:33:38 +0200

    fix: Move test closer to the component. Fix the test itself.

---
+++ src/hooks/ComponentWithState.test.tsx
@@ -0,0 +1,22 @@
+/*
+  @see https://github.com/testing-library/react-testing-library
+*/
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import ComponentWithState from './ComponentWithState';
+
+describe('Hooks', () => {
+	test('ComponentWithState', async () => {
+		render(<ComponentWithState />);
+
+		const button = screen.getByRole('button');
+		expect(button).toHaveTextContent('Counter: 0');
+
+		await userEvent.click(button);
+		expect(button).toHaveTextContent('Counter: 1');
+
+		await userEvent.click(button);
+		expect(button).toHaveTextContent('Counter: 2');
+	});
+});
