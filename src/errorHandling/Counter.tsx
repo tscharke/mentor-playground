cc1915e4210ca52b414e6f39a6b1c881ec03c5d3
Thomas Scharke <scharke@van.dermeer.de>
2023-10-05 17:38:05 +0200

    fix: The newer version provides the `useErrorBoundary`-hook with a different function

--- src/errorHandling/Counter.tsx
+++ src/errorHandling/Counter.tsx
@@ -1,56 +1,49 @@
 import { useState } from 'react';
-import { useErrorHandler } from 'react-error-boundary';
+import { useErrorBoundary } from 'react-error-boundary';
 import { Mode } from './ErrorHandlingDemo';
 
 type CounterProperties = {
-  maxCount: number;
+	maxCount: number;
 };
 
 const CounterNormalMode = ({ maxCount }: CounterProperties) => {
-  const [counter, setCounter] = useState(0);
-  const handleError = (error: any) => console.error('CounterNormalMode', error);
+	const [counter, setCounter] = useState(0);
+	const handleError = (error: any) => console.error('CounterNormalMode', error);
 
-  const incrementCounter = () => {
-    try {
-      if (counter === maxCount) {
-        throw Error('The Counter-Component reached the max count');
-      } else {
-        setCounter((c) => c + 1);
-      }
-    } catch (error) {
-      handleError(error);
-    }
-  };
+	const incrementCounter = () => {
+		try {
+			if (counter === maxCount) {
+				throw Error('The Counter-Component reached the max count');
+			} else {
+				setCounter((c) => c + 1);
+			}
+		} catch (error) {
+			handleError(error);
+		}
+	};
 
-  return <button onClick={incrementCounter}>Count {counter}</button>;
+	return <button onClick={incrementCounter}>Count {counter}</button>;
 };
 
 const CounterExtendedMode = ({ maxCount }: CounterProperties) => {
-  const [counter, setCounter] = useState(0);
-  const handleError = useErrorHandler();
+	const [counter, setCounter] = useState(0);
+	const { showBoundary } = useErrorBoundary();
 
-  const incrementCounter = () => {
-    try {
-      if (counter === maxCount) {
-        throw Error('The CounterExtendedMode-Component reached the max count');
-      } else {
-        setCounter((c) => c + 1);
-      }
-    } catch (error) {
-      handleError(error);
-    }
-  };
+	const incrementCounter = () => {
+		try {
+			if (counter === maxCount) {
+				throw Error('The CounterExtendedMode-Component reached the max count');
+			} else {
+				setCounter((c) => c + 1);
+			}
+		} catch (error) {
+			showBoundary(error);
+		}
+	};
 
-  return <button onClick={incrementCounter}>Count {counter}</button>;
+	return <button onClick={incrementCounter}>Count {counter}</button>;
 };
 
-export default function ({
-  mode,
-  ...props
-}: CounterProperties & { mode: Mode }) {
-  return mode === 'normal' ? (
-    <CounterNormalMode {...props} />
-  ) : (
-    <CounterExtendedMode {...props} />
-  );
+export default function ({ mode, ...props }: CounterProperties & { mode: Mode }) {
+	return mode === 'normal' ? <CounterNormalMode {...props} /> : <CounterExtendedMode {...props} />;
 }
