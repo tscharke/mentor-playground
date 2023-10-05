82c88ca1a61f6c77a0d879f4197b58bec7453a37
Thomas Scharke <scharke@van.dermeer.de>
2023-10-05 17:32:16 +0200

    chore: Update all dependencies and separate them into dev-dependencies

--- src/app/Overview.tsx
+++ src/app/Overview.tsx
@@ -1,22 +1,22 @@
 import { Link } from 'react-router-dom';
 
 const LINKS = [
-  { link: 'components', name: 'Simple Components' },
-  { link: 'hooks', name: 'Hooks' },
-  { link: 'styled-components', name: 'Styled Components' },
-  { link: 'playgrounds', name: 'Playgrounds' },
-  { link: 'forms', name: 'Forms' },
-  { link: 'error', name: 'Errors' },
-  { link: 'suspense', name: 'Suspense' },
-  { link: 'book-app', name: 'Workshops.de - Book-App' }
+	{ link: 'components', name: 'Simple Components' },
+	{ link: 'hooks', name: 'Hooks' },
+	{ link: 'styled-components', name: 'Styled Components' },
+	{ link: 'playgrounds', name: 'Playgrounds' },
+	{ link: 'forms', name: 'Forms' },
+	{ link: 'errors', name: 'Errors' },
+	{ link: 'suspense', name: 'Suspense' },
+	{ link: 'book-app', name: 'Workshops.de - Book-App' },
 ];
 
 export const Overview = () => (
-  <ul className="Overview">
-    {LINKS.map(({ link, name }) => (
-      <li key={link}>
-        <Link to={link}>{name}</Link>
-      </li>
-    ))}
-  </ul>
+	<ul className="Overview">
+		{LINKS.map(({ link, name }) => (
+			<li key={link}>
+				<Link to={link}>{name}</Link>
+			</li>
+		))}
+	</ul>
 );
