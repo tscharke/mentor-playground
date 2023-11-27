854b0d7dc3a436f0a65c8da2a4b45a0f40bcfec1
Thomas Scharke <scharke@van.dermeer.de>
2023-11-27 16:14:51 +0100

    fix(Vite): Use the correct heading

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -1,28 +1,28 @@
-import { Outlet, useNavigate, useLocation } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 export const Layout = () => {
-  const navigation = useNavigate();
-  let { pathname } = useLocation();
-  return (
-    <>
-      <div className="App">
-        <h1>TypeScript, JavaScript & React</h1>
-        <h2>by Thomas Scharke</h2>
-      </div>
-      {pathname !== '/' && (
-        <nav>
-          <div
-            onClick={() => {
-              navigation('/');
-            }}
-          >
-            Go home
-          </div>
-        </nav>
-      )}
-      <div className="Outlet">
-        <Outlet />
-      </div>
-    </>
-  );
+	const navigation = useNavigate();
+	let { pathname } = useLocation();
+	return (
+		<>
+			<div className="App">
+				<h1>React & TypeScript</h1>
+				<h2>Mentor Playground</h2>
+			</div>
+			{pathname !== '/' && (
+				<nav>
+					<div
+						onClick={() => {
+							navigation('/');
+						}}
+					>
+						Go home
+					</div>
+				</nav>
+			)}
+			<div className="Outlet">
+				<Outlet />
+			</div>
+		</>
+	);
 };
